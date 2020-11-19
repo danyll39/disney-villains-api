bfa3fe87735cb92eb65d8933f432345753802c71
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-19 00:28:37 -0500

    Adds an id and updated attribute to fix the id not showing up when a new post is made

--- models/villains.js
+++ models/villains.js
@@ -1,13 +1,13 @@
 const villains = (connection, Sequelize) => {
   return connection.define('villains', {
-
+    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: Sequelize.STRING },
     movie: { type: Sequelize.STRING },
-    slug: { type: Sequelize.STRING, autoIncrement: true, primaryKey: true }, // autoIncrement ??
+    slug: { type: Sequelize.STRING } // autoIncrement ??
 
   }, {
     defaultScope: {
-      attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] } // change to include?
+      attributes: { exclude: ['id', 'createdAt', 'updatedAt', 'deletedAt'] } // change to include?
     }
   }, { paranoid: true })
 }
