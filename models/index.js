f3cf3d2e424ce2ddd24c10a72f1ce5144c1fe60a
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-22 21:32:33 -0500

    Remove pseudocode and whitespace

--- models/index.js
+++ models/index.js
@@ -1,12 +1,9 @@
 const Sequelize = require('sequelize')
 const villainsModel = require('./villains')
 
-const connection = new Sequelize('disney'// database
-  , 'villains'// name of user
-  , 'Vi11Ains!'// password
-  , {
-    host: 'localhost', dialect: 'mysql'
-  })
+const connection = new Sequelize('disney', 'villains', 'Vi11Ains!', {
+  host: 'localhost', dialect: 'mysql'
+})
 const villains = villainsModel(connection, Sequelize)
 
 module.exports = { villains }
